1bd795ac25403c761905c71fb27ab71c62d8b006
Alex Royce <alexroyce315@users.noreply.github.com>
2020-05-30 11:55:58 +0800

    Update config.php

--- application/config/config.php
+++ application/config/config.php
@@ -523,5 +523,14 @@ $config['rewrite_short_tags'] = FALSE;
 */
 $config['proxy_ips'] = '';
 
-# HMVC enable
+/*
+|--------------------------------------------------------------------------
+| Modules locations
+|--------------------------------------------------------------------------
+|
+| These are the folders where your modules are located. You may define an
+| absolute path to the location or a relative path starting from the root
+| directory.
+|
+*/
 $config['modules_locations'] = array(APPPATH . 'modules/');
